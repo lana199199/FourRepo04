d7f225e40d7a4d61d498abc1094de34ec2af95cf
thanh.pnguyen <thanh.pnguyen@GXHW2D3>
2022-06-22 19:42:22 +0700

    4th commit

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>c89f437f-982e-4a32-b9ca-7336d703ba7e</testSuiteGuid>
+</TestSuiteEntity>
